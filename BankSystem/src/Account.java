0defdb65770e454a30a036a0343fd19dae37f85a
JVKszuba <jvkaszuba@gmail.com>
2025-05-27 09:38:24 -0300

    FEAT: account class and execution program created

---
+++ BankSystem/src/Account.java
@@ -0,0 +1,54 @@
+public class Account {
+
+    private final int id;
+    private String holderName;
+    private double balance;
+
+    public Account(int id, String holderName, double balance) {
+
+        this.id = id;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    public Account(int id, String holderName) {
+
+        this.id = id;
+        this.holderName = holderName;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setHolderName(String holderName) {
+
+        this.holderName = holderName;
+    }
+
+    public String getHolderName() {
+
+        return holderName;
+    }
+
+    public double getBalance() {
+
+        return balance;
+    }
+
+    public void makeDeposit(double amount) {
+
+        this.balance += amount;
+    }
+
+    public void makeWithdraw(double amount) {
+
+        this.balance -= (amount + 5);
+    }
+
+    public String toString() {
+
+        return "Account = " + getId() + ", Holder = " + getHolderName() + ", Balance = $ " + getBalance() + "\n";
+    }
+}
--- BankSystem/src/Main.java
+++ BankSystem/src/Main.java
@@ -1,7 +1,54 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Account account;
+        String name;
+        char answer;
+        double value;
+        int id;
+
+        System.out.print("Enter account number: ");
+        id = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.print("Enter account holder: ");
+        name = sc.nextLine();
+
+        do {
+
+            System.out.print("Is there an initial deposit (y/n)?");
+            answer = sc.nextLine().toLowerCase().charAt(0);
+
+        } while (answer != 'y' && answer != 'n');
+
+        if (answer == 'y') {
+
+            System.out.print("Enter initial deposit amount: ");
+            value = sc.nextDouble();
+
+            account = new Account(id, name, value);
+
+        } else account = new Account(id, name);
+
+        System.out.println("\nAccount data:\n" + account);
+
+        System.out.print("Enter a deposit amount: ");
+        value = sc.nextDouble();
+        account.makeDeposit(value);
+        System.out.println("Updated account data:\n" + account);
+
+        System.out.print("Enter a withdrawal amount: ");
+        value = sc.nextDouble();
+        account.makeWithdraw(value);
+        System.out.println("Updated account data:\n" + account);
+
+        sc.close();
     }
 }
