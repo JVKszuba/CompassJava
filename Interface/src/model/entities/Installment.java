297ef525bc47744354eda4fc98c645bebf2247c4
JVKszuba <jvkaszuba@gmail.com>
2025-06-10 10:53:43 -0300

    FEAT: create contract and installment entities

---
+++ Interface/src/model/entities/Installment.java
@@ -0,0 +1,31 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Installment {
+
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    LocalDate dueDate;
+    Double amount;
+
+    public Installment() {}
+    public Installment(LocalDate dueDate, Double amount) {
+
+        this.dueDate = dueDate;
+        this.amount = amount;
+    }
+
+    public LocalDate getDueDate() {return dueDate;}
+    public Double getAmount() {return amount;}
+
+    public void setDueDate(LocalDate dueDate) {this.dueDate = dueDate;}
+    public void setAmount(Double amount) {this.amount = amount;}
+
+    @Override
+    public String toString() {
+
+        return dueDate.format(dtf) + " - " + String.format("%.2f", amount);
+    }
+}
