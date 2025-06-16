f4c11440122bfd9f680a1c38ed34c8655b728c82
JVKszuba <jvkaszuba@gmail.com>
2025-06-16 12:10:54 -0300

    FEAT: create user service

--- SpringBoot/src/main/java/com/compass/springboot/resources/UserResource.java
+++ SpringBoot/src/main/java/com/compass/springboot/resources/UserResource.java
@@ -1,19 +1,35 @@
 package com.compass.springboot.resources;
 
 import com.compass.springboot.entities.User;
+import com.compass.springboot.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll() {
+    @Autowired
+    private UserService service;
 
-        User user = new User(1L, "Maria", "maria@gmail.com", "9999999999", "12345");
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+
+        List<User> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable(value = "id") Long id) {
+
+        User user = service.findById(id);
 
         return ResponseEntity.ok().body(user);
     }
---
+++ SpringBoot/src/main/java/com/compass/springboot/services/UserService.java
@@ -0,0 +1,27 @@
+package com.compass.springboot.services;
+
+import com.compass.springboot.entities.User;
+import com.compass.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+
+        return repository.findAll();
+    };
+
+    public User findById(Long id) {
+
+        return repository.findById(id).get();
+    }
+}
