64228969de8532a26b6c18e6f7b767463df89558
JVKszuba <jvkaszuba@gmail.com>
2025-06-17 14:25:17 -0300

    FEAT: add one-to-many relationship between product and order item

--- SpringBoot/src/main/java/com/compass/springboot/entities/Product.java
+++ SpringBoot/src/main/java/com/compass/springboot/entities/Product.java
@@ -1,5 +1,6 @@
 package com.compass.springboot.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -23,6 +24,9 @@ public class Product implements Serializable {
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product() {}
     public Product(Long id, String name, String description, Double price, String imgUrl) {
 
@@ -39,6 +43,15 @@ public class Product implements Serializable {
     public Double getPrice() {return price;}
     public String getImgUrl() {return imgUrl;}
     public Set<Category> getCategories() {return categories;}
+    @JsonIgnore
+    public Set<Order> getOrders() {
+
+        Set<Order> set = new HashSet<>();
+
+        for (OrderItem orderItem : items) {set.add(orderItem.getOrder());}
+
+        return set;
+    }
 
     public void setId(Long id) {this.id = id;}
     public void setName(String name) {this.name = name;}
