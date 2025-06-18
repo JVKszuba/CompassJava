90bdc7e5632846b9e79829896d4250b7f59274a6
JVKszuba <jvkaszuba@gmail.com>
2025-06-18 09:58:51 -0300

    FEAT: add exception handling for database deletion error

--- SpringBoot/src/main/java/com/compass/springboot/services/UserService.java
+++ SpringBoot/src/main/java/com/compass/springboot/services/UserService.java
@@ -2,8 +2,11 @@ package com.compass.springboot.services;
 
 import com.compass.springboot.entities.User;
 import com.compass.springboot.repository.UserRepository;
+import com.compass.springboot.services.exceptions.DatabaseException;
 import com.compass.springboot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
@@ -33,7 +36,9 @@ public class UserService {
 
     public void delete(Long id) {
 
-        repository.deleteById(id);
+        try{repository.deleteById(id);}
+        catch (EmptyResultDataAccessException e) {throw new ResourceNotFoundException(id);}
+        catch (DataIntegrityViolationException e) {throw new DatabaseException(e.getMessage());}
     }
 
     public User update(Long id, User user) {
