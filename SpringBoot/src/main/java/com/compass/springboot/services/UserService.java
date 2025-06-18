1eae2a4c88f4bd42c790a1336a4b9676e86a77b4
JVKszuba <jvkaszuba@gmail.com>
2025-06-18 09:27:09 -0300

    FEAT: add PUT endpoint to update user data

--- SpringBoot/src/main/java/com/compass/springboot/services/UserService.java
+++ SpringBoot/src/main/java/com/compass/springboot/services/UserService.java
@@ -34,4 +34,20 @@ public class UserService {
 
         repository.deleteById(id);
     }
+
+    public User update(Long id, User user) {
+
+        User entity = repository.getReferenceById(id);
+
+        updateData(entity, user);
+
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User user) {
+
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setCellphone(user.getCellphone());
+    }
 }
